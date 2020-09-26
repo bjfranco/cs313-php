2a3a7fbe50494683107e9144e8a2cbff936b86c8
bjfranco <fra18020@byui.edu>
2020-09-26 13:43:42 -0700

    added contact links to homepage

--- web/home.php
+++ web/home.php
@@ -21,6 +21,23 @@
 			<img src="family.jpg" alt="Family Photo" class="responsive">
 		</div>
 		<div class="column">
+
+			<h1>Contact Me</h1>
+			<div class="flex-container">
+				<a href="https://www.linkedin.com/in/brigham-francois/" target="blank">
+					<img src="linkedin.png" height="50px" width="50px">
+				</a>
+				<a href="mailto:fra18020@byui.edu" target="_blank">
+					<img src="gmail.png" height="50px" width="50px">
+				</a>
+				<a href="https://www.instagram.com/brigfrancois/" target="_blank">
+					<img src="instagram.png" height="50px" width="50px">
+				</a>
+				<a href="https://www.facebook.com/brig.francois.3/" target="_blank">
+					<img src="facebook.png" height="50px" width="50px">
+				</a>
+			</div>
+            
 			<h1>Server Time</h1>
 			<?php
 			echo "The time is " . date("h:i:sa");
