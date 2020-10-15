cfca22b07e40da68225ea56cb11cce68cca6820a
bjfranco <fra18020@byui.edu>
2020-10-14 17:37:40 -0700

    Added database connection in search.php

--- web/prove5/search.php
+++ web/prove5/search.php
@@ -16,22 +16,51 @@
 
 	<div>
 		<h2>Bird Search</h2>
-		<form>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
 			<label for="bird">Bird Name:</label><br>
 			<input type="text" name="bird" id="bird"><br><br>
 			<input type="submit" name="submit" value="Search">
 		</form>
 
 		<h2>Location Search</h2>
-		<form>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 			<label for="city">City:</label><br>
 			<input type="text" name="city" id="city"><br>
+
 			<label for="state">State:</label><br>
 			<input type="text" name="state" id="state"><br>
+
 			<label for="country">Country:</label><br>
 			<input type="text" name="country" id="country"><br><br>
+			
 			<input type="submit" name="submit" value="Search">
 		</form>
+
+			<?php
+			    try
+		        {
+		        $dbUrl = getenv('DATABASE_URL');
+		        
+		        $dbOpts = parse_url($dbUrl);
+		        
+		        $dbHost = $dbOpts["host"];
+		        $dbPort = $dbOpts["port"];
+		        $dbUser = $dbOpts["user"];
+		        $dbPassword = $dbOpts["pass"];
+		        $dbName = ltrim($dbOpts["path"],'/');
+		        
+		        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		        
+		        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		        }
+		        catch (PDOException $ex)
+		        {
+		        echo 'Error!: ' . $ex->getMessage();
+		        die();
+		        } 
+			?>
+
 	</div>
 
 </body>
