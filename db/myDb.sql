8ec45f308e1da07f896390ef3d4c009e464460dc
bjfranco <fra18020@byui.edu>
2020-10-14 17:02:04 -0700

    Corrected Table Numbering with Serial

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Member (
-	MemberId int NOT NULL PRIMARY KEY,
+	MemberId serial NOT NULL PRIMARY KEY,
 	FirstName varchar(80) NOT NULL,
 	LastName varchar(80) NOT NULL
 );
 
 CREATE TABLE Bird (
-	BirdId int NOT NULL PRIMARY KEY,
+	BirdId serial NOT NULL PRIMARY KEY,
 	BirdName varchar(80) NOT NULL
 );
 
 CREATE TABLE Sighting (
-	SightingId int NOT NULL PRIMARY KEY,
+	SightingId serial NOT NULL PRIMARY KEY,
 	MemberId int REFERENCES Member(MemberId),
 	BirdId int REFERENCES Bird(BirdId),
 	City varchar(80) NOT NULL,
