4c79a74d3785051189a7fa44ebca8c8d72e3c35a
bjfranco <fra18020@byui.edu>
2020-10-17 11:32:10 -0700

    Improving Search Formatting

--- web/prove5/search.php
+++ web/prove5/search.php
@@ -14,6 +14,31 @@
 		</ul>
 	</header>
 
+
+	<?php
+	    try
+        {
+        $dbUrl = getenv('DATABASE_URL');
+        
+        $dbOpts = parse_url($dbUrl);
+        
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+        
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+        } 
+    ?>
+
 	<div class="search">
 		<h2>Bird Search</h2><br>
 		<p>Enter the name of the bird you are looking for and a record will be displayed showing where and when this bird has been seen.</p>
@@ -24,75 +49,27 @@
 			<input type="submit" name="submit" value="Search">
 		</form><br>
 
-<!--
-		<h2>Location Search</h2>
-			<label for="city">City:</label><br>
-			<input type="text" name="city" id="city"><br>
-
-			<label for="state">State:</label><br>
-			<input type="text" name="state" id="state"><br>
-
-			<label for="country">Country:</label><br>
-			<input type="text" name="country" id="country"><br><br>
-
-			<input type="submit" name="submit" value="Search">
-		</form>
-	-->
+		<?php
+		    $bird = $_POST['bird'];
+		    /*
+		    $city = $_POST['city'];
+		    $state = $_POST['state'];
+		    $country = $_POST['country'];
+		    */
 
 
-			<?php
-			    try
-		        {
-		        $dbUrl = getenv('DATABASE_URL');
-		        
-		        $dbOpts = parse_url($dbUrl);
-		        
-		        $dbHost = $dbOpts["host"];
-		        $dbPort = $dbOpts["port"];
-		        $dbUser = $dbOpts["user"];
-		        $dbPassword = $dbOpts["pass"];
-		        $dbName = ltrim($dbOpts["path"],'/');
-		        
-		        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		        
-		        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		        }
-		        catch (PDOException $ex)
-		        {
-		        echo 'Error!: ' . $ex->getMessage();
-		        die();
-		        } 
-
-
-		        $bird = $_POST['bird'];
-		        $city = $_POST['city'];
-		        $state = $_POST['state'];
-		        $country = $_POST['country'];
-
-/*
-		        if (!empty($bird)) {
-		        	foreach ($db->query('SELECT Bird.birdname, Sighting.city, Sighting.state, Sighting.country, Sighting.sighttime FROM Sighting INNER JOIN Bird ON Bird.birdid = Sighting.birdid') as $row)
-					{
-						if ($_POST['bird'] == $row['birdname']) {
-							echo '<h3>' . $row['birdname'] . '</h3>';
-					  		echo 'Location: ' . $row['city'] . ', ' . $row['state'] . ', ' . $row['country'] . '<br>';
-					  		echo 'Date: ' . $row['sighttime'] . '<br>';
-						}
+		    if (!empty($bird)) {
+		    	echo '<div class="table"><h3>' . $bird . '</h3>';
+		    	echo '<table><tr><th>Date</th><th>City</th><th>State</th><th>Country</th></tr>';
+		    	foreach ($db->query('SELECT Bird.birdname, Sighting.city, Sighting.state, Sighting.country, Sighting.sighttime FROM Sighting INNER JOIN Bird ON Bird.birdid = Sighting.birdid') as $row)
+				{
+					if ($_POST['bird'] == $row['birdname']) {
+				  		echo '<tr><td>' . $row['sighttime'] . '</td><td>' . $row['city'] . '</td><td>' . $row['state'] . '</td><td>' . $row['country'] . '</td></tr>';
 					}
-		        }
-*/
-		        if (!empty($bird)) {
-		        	echo '<div class="table"><h3>' . $bird . '</h3>';
-		        	echo '<table><tr><th>Date</th><th>City</th><th>State</th><th>Country</th></tr>';
-		        	foreach ($db->query('SELECT Bird.birdname, Sighting.city, Sighting.state, Sighting.country, Sighting.sighttime FROM Sighting INNER JOIN Bird ON Bird.birdid = Sighting.birdid') as $row)
-					{
-						if ($_POST['bird'] == $row['birdname']) {
-					  		echo '<tr><td>' . $row['sighttime'] . '</td><td>' . $row['city'] . '</td><td>' . $row['state'] . '</td><td>' . $row['country'] . '</td></tr>';
-						}
-					}
-					echo '</table></div>';
-		        }
-			?>
+				}
+				echo '</table></div>';
+		    }
+		?>
 
 	</div>
 
