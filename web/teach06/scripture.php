936279a0b6857854f8519e1911cea13da35cdfb2
bjfranco <fra18020@byui.edu>
2020-10-19 13:25:38 -0700

    Adding teach06

---
+++ web/teach06/scripture.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripture</title>
+</head>
+<body>
+
+	<form>
+		<label for="book">Book</label>
+		<input type="text" name="book" id="book"><br>
+
+		<label for="chapter">Chapter</label>
+		<input type="text" name="chapter" id="chapter"><br>
+
+		<label for="verse">Verse</label>
+		<input type="text" name="verse" id="verse"><br>
+
+		<label for="content">Book</label>
+		<textarea id="content" name="content" rows="4" cols="50"></textarea>
+
+
+		<?php
+		try
+		{
+		  $dbUrl = getenv('DATABASE_URL');
+
+		  $dbOpts = parse_url($dbUrl);
+
+		  $dbHost = $dbOpts["host"];
+		  $dbPort = $dbOpts["port"];
+		  $dbUser = $dbOpts["user"];
+		  $dbPassword = $dbOpts["pass"];
+		  $dbName = ltrim($dbOpts["path"],'/');
+
+		  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $ex)
+		{
+		  echo 'Error!: ' . $ex->getMessage();
+		  die();
+		}
+
+
+			foreach ($db->query('SELECT topicname FROM topic') as $row)
+			{
+				echo '<input type="checkbox" name="topics" value="' . $row['topicname'] . '" id="' . $row['topicname'] . '"">';
+			}
+		?>
+
+		<input type="submit" name="submit">
+
+	</form>
+
+</body>
+</html>
