42c7d602e376d21713da6c59e1a5a760c4a536df
bjfranco <fra18020@byui.edu>
2020-10-21 11:58:20 -0700

    Formatting Log Page

--- web/prove05/log.php
+++ web/prove05/log.php
@@ -20,19 +20,19 @@
 
 		<form method="post">
 			<label for="bird">Bird Name:</label><br>
-			<input type="text" name="bird" id="bird"><br>
+			<input type="text" name="bird" id="bird" placeholder="Blue Jay"><br>
 
 			<label for="city">City:</label><br>
-			<input type="text" name="city" id="city"><br>
+			<input type="text" name="city" id="city" placeholder="Los Angelos"><br>
 
 			<label for="state">State:</label><br>
-			<input type="text" name="state" id="state"><br>
+			<input type="text" name="state" id="state" placeholder="California"><br>
 
 			<label for="country">Country:</label><br>
-			<input type="text" name="country" id="country"><br>
+			<input type="text" name="country" id="country" placeholder="United States"><br>
 
 			<label for="date">Date:</label><br>
-			<input type="text" name="date" id="date"><br>
+			<input type="text" name="date" id="date" placeholder="YYYY-MM-DD"><br>
 
 			<input type="submit" name="submit" value="Submit"><br>
 			
