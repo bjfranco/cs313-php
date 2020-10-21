400c6af4e35026a2ad2063090b27a4cf52e3aeea
bjfranco <fra18020@byui.edu>
2020-10-21 11:29:05 -0700

    Adding Teach06 Files

--- web/teach06/test.sql
+++ web/teach06/test.sql
@@ -9,13 +9,33 @@ CREATE TABLE scripturetopic (
 	topicid INT REFERENCES topic(id)
 );
 
+
+
 SELECT s.book, s.chapter, s.verse, s.content, st.topicname FROM Scripture s INNER JOIN scripturetopic st ON s.scriptureid = st.scriptureid;
 
 
 
-
+SELECT FROM topic INNER JOIN scripturetopic ON
 
 
 
 
 INSERT INTO topic(topicname) VALUES('Faith'), ('Sacrifice'), ('Charity');
+
+
+foreach ($db->query('SELECT scriptureid, book, chapter, verse, content FROM scripture') as $row)
+
+SELECT t.topicname
+FROM topic AS t 
+INNER JOIN scripturetopic AS st 
+ON st.topicid = t.id
+WHERE st.scriptureid = :scripture;
+
+$stmt = $db->prepare('SELECT t.topicname
+FROM topic AS t 
+INNER JOIN scripturetopic AS st 
+ON st.topicid = t.id
+WHERE st.scriptureid = :scripture;');
+$stmt->bindValue(':scripture', $row['scriptureid'], PDO::PARAM_INT);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
