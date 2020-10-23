f5e937b17abefe52018058238c7df8f91560e01d
bjfranco <fra18020@byui.edu>
2020-10-23 13:05:33 -0700

    Adding SQL Insert to Log.php

--- web/prove05/log.php
+++ web/prove05/log.php
@@ -45,7 +45,7 @@
     function addBird($db, $birdid)
     {
     	$birdentry = htmlspecialchars('$_POST['birdid']');
-    	$stmt = $db->prepare('UPDATE Sighting SET Sighting.birdid = Bird.birdid WHERE Bird.birdname = $birdid');
+    	/*$stmt = $db->prepare('UPDATE Sighting SET Sighting.birdid = Bird.birdid WHERE Bird.birdname = $birdid');*/
     	/*$stmt->execute(array(':birdid' => $birdid));*/
     }
 
