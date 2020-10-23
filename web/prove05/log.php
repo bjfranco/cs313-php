6e46e7463bf80453635d066c0abe6aa9ef3b3f6d
bjfranco <fra18020@byui.edu>
2020-10-22 19:59:09 -0700

    Adding SQL Insert to Log.php

--- web/prove05/log.php
+++ web/prove05/log.php
@@ -23,7 +23,7 @@
 
     function insertLog($db, $bird, $city, $state, $country, $sighttime)
     {
-    	$db->query('SELECT birdid FROM Bird WHERE birdname=' . $_POST['bird'] . '') AS $birdid;
+    	$statement = $db->query('SELECT birdid FROM Bird WHERE birdname=' . $_POST['bird'] . '') AS $birdid;
 
     	$statement = $db->prepare('INSERT INTO Sighting(BirdId, City, State, Country, SightTime) VALUES(:birdid, :city, :state, :country, :sighttime)');
 		$statement->execute(array(':birdid' = $birdid, ':city' => $city, ':state' => $state, ':country' => $country, ':sighttime' => $sighttime));
