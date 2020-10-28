b758869523ffefc4d46069ec8cfd44b2326939df
bjfranco <fra18020@byui.edu>
2020-10-27 18:05:36 -0700

    Updating Styles

--- web/birdwatcher/navbar.php
+++ web/birdwatcher/navbar.php
@@ -1,8 +1,8 @@
 <header class="nav">
 	<ul>
 		<h1>Bird Watcher</h1>
-		<li><a href="log.php"><i class="material-icons">face</i>Log</a></li>
-		<li><a href="search.php">Search</a></li>
-		<li><a href="index.php">Home</a></li>
+		<li><a href="log.php"><i class="material-icons">article</i> Log</a></li>
+		<li><a href="search.php"><i class="material-icons">search</i> Search</a></li>
+		<li><a href="index.php"><i class="material-icons">home</i> Home</a></li>
 	</ul>
 </header>
--- web/birdwatcher/search.php
+++ web/birdwatcher/search.php
@@ -9,6 +9,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
 	<title>Bird Watcher</title>
 </head>
